bf265166dd9f8c926fbc53eb899c346b848a7623
廖伟 <24591106@qq.com>
2019-08-06 14:43:51 +0800

    提交代码

--- src/main/java/cn/net/liaowei/sc/gateway/ScGatewayApplication.java
+++ src/main/java/cn/net/liaowei/sc/gateway/ScGatewayApplication.java
@@ -2,6 +2,9 @@ package cn.net.liaowei.sc.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.filter.FilterDefinition;
+import org.springframework.cloud.gateway.handler.predicate.PredicateDefinition;
 
 @SpringBootApplication
 public class ScGatewayApplication {
